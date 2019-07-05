310c14bf3e4a1af410c8a0c1dbbf831144e8db04
ciro.azevedo <ciro.azevedo@4all.com>
2019-07-05 11:28:54 -0300

    Ajustes no dotenv

--- src/env.ts
+++ src/env.ts
@@ -1,17 +1,16 @@
 import * as dotenv from 'dotenv';
 
+const result: dotenv.DotenvConfigOutput = dotenv.config();
 
-
-
-const result = dotenv.config();
-
-
+if (result.error) {
+  throw result.error;
+}
 
 export default {
   DB_HOST: process.env.DB_HOST,
-  DB_USERNAME: process.env.DB_USERNAME.
+  DB_USERNAME: process.env.DB_USERNAME,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_DATABASE: process.env.DB_DATABASE,
   DB_POOL_MIN: process.env.DB_POOL_MIN,
-  DB_POOL_MAX: process.env.DB_POOL_MAX,
+  DB_POOL_MAX: process.env.DB_POOL_MAX
 }
