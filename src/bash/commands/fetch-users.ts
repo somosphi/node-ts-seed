823515190e1d26055e4955bcd40a51fdd0b56b07
Giuseppe Menti <mentifg@gmail.com>
2019-07-22 08:49:05 -0300

    Add bash and command example

---
+++ src/bash/commands/fetch-users.ts
@@ -0,0 +1,17 @@
+import { Command } from './command';
+import { Container } from '../../container';
+import { UserService } from '../../container/services/user';
+
+export class FetchUsersCommand extends Command {
+  signature: string = 'fetch-users';
+  protected readonly userService: UserService;
+
+  constructor(container: Container) {
+    super();
+    this.userService = container.userService;
+  }
+
+  async handle(): Promise<void> {
+    await this.userService.fetchFromJsonPlaceholder();
+  }
+}
